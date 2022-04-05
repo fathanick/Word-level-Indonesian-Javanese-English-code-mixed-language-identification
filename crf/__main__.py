7083baaa528ed747688574535afd961c6dee2f34
Fathan <fathanel@gmail.com>
2022-04-05 13:23:20 +0800

    New commit

--- crf/__main__.py
+++ crf/__main__.py
@@ -6,8 +6,8 @@ sys.path.insert(0, '../helper')
 
 langid = LanguageIdentifier()
 
-#data = read_tsv('../raw dataset/all-tagged-280322.tsv')
-data = read_tsv('../raw dataset/all-tagged-280322-v2.tsv')
+data = read_tsv('../raw dataset/all-tagged-280322.tsv')
+#data = read_tsv('../raw dataset/all-tagged-280322-v2.tsv')
 # Scenario 1: 60:40
 #print('Scenario 1')
 #langid.pipeline(data, test_size=0.4, model_name='mod_6_4_dtv2.pkl')
@@ -18,5 +18,5 @@ data = read_tsv('../raw dataset/all-tagged-280322-v2.tsv')
 
 # Scenario 3: 80:20
 print('\n\n Scenario 3')
-langid.pipeline(data, test_size=0.2, model_name='mod_8_2_dtv2.pkl')
+langid.pipeline(data, test_size=0.2, model_name='mod_8_2.pkl')
 
